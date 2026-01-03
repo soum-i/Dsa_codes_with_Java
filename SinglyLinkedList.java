8aafc4a06c698a289e7911f83550e41ae9a8cc58
soumi <soumi3001@gmail.com>
2026-01-03 11:14:39 +0530

    Added new operations

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -9,35 +9,68 @@ public class SinglyLinkedList {
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
 
+        System.out.println("The Linked List");
         traversal(head);
         System.out.println();
 
-        System.out.print("Element to be added at the first: ");
-        int input = sc.nextInt();
-        head = insertAtFront(head, input);
+        System.out.print("Do you want to perform any operation(y/n): ");
+        char ans = sc.next().charAt(0);
 
-        System.out.println("The New Array");
-        traversal(head);
+        while (ans == 'y' || ans == 'Y') {
+            System.out.println("\nPress a number to perform that operation");
+            System.out.println("1. Insert At First");
+            System.out.println("2. Insert At End");
+            System.out.println("3. Insert At Any Position");
+            System.out.println("4. Delete The First Node");
+            System.out.println("5. Delete the last Node");
+            System.out.println("6. Delete a node from any position");
 
-        System.out.println();
-        System.out.print("Element to be added at the end: ");
-        int end = sc.nextInt();
-        head = insertAtEnd(head, end);
+            int newAns = sc.nextInt();
 
-        System.out.println("the new array");
-        traversal(head);
-        System.out.println();
+            switch (newAns) {
+                case 1:
+                    System.out.print("Element to be added at the first: ");
+                    head = insertAtFront(head, sc.nextInt());
+                    break;
 
-        System.out.print("add element to a specific position(value, position): ");
-        int value = sc.nextInt();
-        int pos = sc.nextInt();
-        head = insertAtPos(head, pos, value);
+                case 2:
+                    System.out.print("Element to be added at the end: ");
+                    head = insertAtEnd(head, sc.nextInt());
+                    break;
 
-        System.out.println("the new array");
-        traversal(head);
-        System.out.println();
+                case 3:
+                    System.out.print("Enter value and position: ");
+                    int value = sc.nextInt();
+                    int pos = sc.nextInt();
+                    head = insertAtPos(head, pos, value);
+                    break;
+
+                case 4:
+                    head = deleteHead(head);
+                    break;
+
+                case 5:
+                    head = deleteLast(head);
+                    break;
+
+                case 6:
+                    System.out.println("Enter the position: ");
+                    int position = sc.nextInt();
+                    head = deletePos(head, position);
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
+
+            System.out.println("Updated Linked List:");
+            traversal(head);
+            System.out.println();
+
+            System.out.print("Do you want to perform another operation? (y/n): ");
+            ans = sc.next().charAt(0);
+        }
     }
-
     //function for adding a new node at index 0
     static Node insertAtFront(Node head, int n) {
         Node newNode = new Node(n);
@@ -71,6 +104,46 @@ public class SinglyLinkedList {
         Node newNode = new Node(n);
         newNode.next = curr.next;
         curr.next = newNode;
+        return head;
+    }
+
+    //function to delete the head
+    static Node deleteHead(Node head) {
+        if(head == null)
+            return null;
+        Node temp = head;
+        head = head.next;
+        temp = null;
+        return head;
+    }
+
+    //function to delete the last node
+    static Node deleteLast(Node head) {
+        if(head == null || head.next == null) {
+            return null;
+        }
+        Node curr = head;
+        while(curr.next.next != null) {
+            curr = curr.next;
+        }
+        curr.next = null;
+        return head;
+    }
+
+    //function to delete a node from any position
+    static Node deletePos(Node head, int pos) {
+        Node temp = head;
+
+        if(pos == 1) {
+            head = temp.next;
+            return head;
+        }
+        Node prev = null;
+        for(int i = 1; i < pos; i++) {
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = temp.next;
         return head;
     }
 
