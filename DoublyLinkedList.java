34dd55a8841d5115bf89e8d04914dca2fa7abd28
soumi <soumi3001@gmail.com>
2026-01-04 12:42:56 +0530

    Adding the method to insert a node at the front and at the end

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -20,14 +20,35 @@ public class DoublyLinkedList {
         head.next.next.prev = head.next;
         head.next.next.next = new Node1(40);
         head.next.next.next.prev = head.next.next;
+
+        System.out.println("The default Linked List");
         traversal1(head);
         System.out.println();
-        System.out.print("Element you wanna insert at the front: ");
-        int data = sc.nextInt();
-        head = insertAtFirst(head, data);
-        System.out.println("Updated Linked List");
-        traversal1(head);
 
+        char ans = 'y';
+        while(ans == 'y' || ans == 'Y') {
+            System.out.println("Press a number to perform an operation.");
+            System.out.println("1. Insertion at the front");
+            System.out.println("2. Insertion at the end");
+            System.out.print("Your choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Element you wanna insert at the front: ");
+                    head = insertAtFirst(head, sc.nextInt());
+                    break;
+                case 2:
+                    System.out.print("Element you wanna insert at the end: ");
+                    head = insertAtEnd(head, sc.nextInt());
+                    break;
+            }
+            System.out.println("Updated Linked List");
+            traversal1(head);
+            System.out.println();
+            System.out.print("Perform more operations?: ");
+            ans = sc.next().charAt(0);
+        }
     }
 
     static Node1 insertAtFirst(Node1 head, int n) {
@@ -37,6 +58,22 @@ public class DoublyLinkedList {
             head.prev = newNode;
         }
         return newNode;
+    }
+
+    static Node1 insertAtEnd(Node1 head, int n) {
+        Node1 newNode = new Node1(n);
+        if(head == null) {
+            head = newNode;
+        }
+        else {
+            Node1 curr = head;
+            while(curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = newNode;
+            newNode.prev = curr;
+        }
+        return head;
     }
     static void traversal1(Node1 head) {
         Node1 temp = head;
