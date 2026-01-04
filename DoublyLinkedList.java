7e7d22ab5b352a42ee7316f4affbf57b5867d70b
soumi <soumi3001@gmail.com>
2026-01-04 12:22:25 +0530

    Adding the method to insert a node at the front

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Node1 {
     int data;
     Node1 prev;
@@ -10,6 +12,7 @@ class Node1 {
 
 public class DoublyLinkedList {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Node1 head = new Node1(10);
         head.next = new Node1(20);
         head.next.prev = head;
@@ -18,7 +21,22 @@ public class DoublyLinkedList {
         head.next.next.next = new Node1(40);
         head.next.next.next.prev = head.next.next;
         traversal1(head);
+        System.out.println();
+        System.out.print("Element you wanna insert at the front: ");
+        int data = sc.nextInt();
+        head = insertAtFirst(head, data);
+        System.out.println("Updated Linked List");
+        traversal1(head);
 
+    }
+
+    static Node1 insertAtFirst(Node1 head, int n) {
+        Node1 newNode = new Node1(n);
+        newNode.next = head;
+        if(head != null) {
+            head.prev = newNode;
+        }
+        return newNode;
     }
     static void traversal1(Node1 head) {
         Node1 temp = head;
